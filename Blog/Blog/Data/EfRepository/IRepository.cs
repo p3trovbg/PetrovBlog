8f24b0daabcf10c75a2ac0b63e887981e5d81f3f
George <p3trovsit@gmail.com>
2023-02-01 17:28:37 +0200

    Add repository

---
+++ Blog/Blog/Data/EfRepository/IRepository.cs
@@ -0,0 +1,16 @@
+﻿namespace Blog.Data.EfRepository
+{
+    public interface IRepository<TEntity> : IDisposable 
+        where TEntity : class
+    {
+        int Count { get; }
+
+        Task AddAsync(TEntity entity);
+
+        Task<int> SaveChangesAsync();
+
+        IQueryable<TEntity> AllAsNoTracking();
+
+        IQueryable<TEntity> All();
+    }
+}
---
+++ Blog/Blog/Data/EfRepository/Repository.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace Blog.Data.EfRepository
+{
+    public class Repository<TEntity> : IRepository<TEntity>
+        where TEntity : class
+    {
+        public Repository(ApplicationDbContext context)
+        {
+            this.Context = context ?? throw new ArgumentNullException(nameof(context));
+            this.DbSet = this.Context.Set<TEntity>();
+        }
+
+        protected DbSet<TEntity> DbSet { get; set; }
+
+        protected ApplicationDbContext Context { get; set; }
+
+        public int Count => this.DbSet.Count();
+
+        public Task AddAsync(TEntity entity) => this.DbSet.AddAsync(entity).AsTask();
+
+        public IQueryable<TEntity> All() => this.DbSet;
+
+        public IQueryable<TEntity> AllAsNoTracking() => this.DbSet.AsNoTracking();
+
+        public Task<int> SaveChangesAsync() => this.Context.SaveChangesAsync();
+
+        public void Dispose()
+        {
+            this.Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                this.Context?.Dispose();
+            }
+        }
+
+    }
+}
