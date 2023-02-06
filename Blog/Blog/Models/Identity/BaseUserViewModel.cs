2b7a25227736647809cfe4b5e8e93045ce5f9697
George <p3trovsit@gmail.com>
2023-02-06 11:30:23 +0200

    Add base post view model

---
+++ Blog/Blog/Models/Identity/BaseUserViewModel.cs
@@ -0,0 +1,7 @@
+﻿namespace Blog.Models.Identity
+{
+    public class BaseUserModel
+    {
+        public string? UserName { get; set; }
+    }
+}
---
+++ Blog/Blog/Models/Post/BasePostViewModel.cs
@@ -0,0 +1,19 @@
+﻿using Blog.Models.Identity;
+
+namespace Blog.Models.Post
+{
+    public class BasePostViewModel
+    {
+        public string Id { get; set; } = default!;
+
+        public string Title { get; set; } = default!;
+
+        public string? MainImageUrl { get; set; }
+
+        public BaseUserViewModel Author { get; set; } = default!;
+
+        public DateTime CreatedOn { get; set; }
+
+        public DateTime? UpdatedOn { get; set; }
+    }
+}
