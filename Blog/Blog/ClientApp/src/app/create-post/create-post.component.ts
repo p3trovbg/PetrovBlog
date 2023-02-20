694d18455e6f10c848c235bc0f25cdbb3837121b
George <p3trovsit@gmail.com>
2023-02-20 20:16:31 +0200

    adding create post component

--- Blog/Blog/ClientApp/src/app/create-post/create-post.component.ts
+++ Blog/Blog/ClientApp/src/app/create-post/create-post.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { Router } from '@angular/router';
+import { PostService } from '../common/post.service';
 
 @Component({
   selector: 'app-create-post',
@@ -9,14 +11,79 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 export class CreatePostComponent {
   createForm: FormGroup;
   errorMessage = '';
-  constructor(private fb: FormBuilder) {
-    this.createForm = fb.group({
+  selectedImageIdx: number = 0;
+  selectedElement: any;
+  imageFiles?: File[] = [];
+  imageUrls?: any[] = [];
+
+  constructor(private router: Router ,private fb: FormBuilder, private postService: PostService) {
+    this.createForm = this.fb.group({
       'title': ['', [Validators.required]],
       'content': ['', [Validators.required]],
-      'mainImage': [''],
+      'mainImage': ['', [Validators.required]],
       'images': [''],
       'videos': [''],
     })
   }
 
+  createPost() {
+    // this.createForm.get('images')?.setValue(this.imageFiles);
+
+    const newPost = new FormData();
+    newPost.append('title', this.createForm.value.title);
+    newPost.append('content', this.createForm.value.content);
+    newPost.append('mainImage', this.createForm.value.mainImage);
+
+    this.imageFiles?.forEach(image => {   
+
+      if (this.imageFiles![this.selectedImageIdx] !== image) {
+        newPost.append('images', image);
+      }
+    });
+    this.postService.add(newPost)
+    .subscribe(postId => this.router.navigateByUrl(`/detail/${postId}`));
+  }
+
+  mainImageHandle(url: any) {
+    this.selectedElement = url;
+    const currentIndex = this.imageUrls?.indexOf(url);
+    this.selectedImageIdx = currentIndex!;
+    const currentFile = this.imageFiles![currentIndex!];
+    this.createForm.get('mainImage')?.setValue(currentFile);
+  }
+  
+  onFileChange(event: any) {
+    [...event.target.files!].forEach((file) => {
+      this.imageFiles?.push(file);
+      var reader = new FileReader();
+		  reader.readAsDataURL(file); 
+      reader.onload = (_event) => {
+      this.imageUrls?.push(reader.result!);
+      }
+    });
+  }
+
+  isSelected(element: any) {
+    return element == this.selectedElement;
+  }
+
+  get username() {
+    return this.createForm.get('title');
+  }
+  
+  get content() {
+    return this.createForm.get('content');
+  }
+
+  get mainImage() {
+    return this.createForm.get('mainImage');
+  }
+
+  get images() {
+    return this.createForm.get('images');
+  }
+
+  get videos() {
+    return this.createForm.get('videos');
+  }
 }
--- Blog/Blog/ClientApp/src/app/create-post/dist/create-post.component.js
+++ Blog/Blog/ClientApp/src/app/create-post/dist/create-post.component.js
@@ -5,22 +5,109 @@ var __decorate = (this && this.__decorate) || function (decorators, target, key,
     else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
     return c > 3 && r && Object.defineProperty(target, key, r), r;
 };
+var __spreadArrays = (this && this.__spreadArrays) || function () {
+    for (var s = 0, i = 0, il = arguments.length; i < il; i++) s += arguments[i].length;
+    for (var r = Array(s), k = 0, i = 0; i < il; i++)
+        for (var a = arguments[i], j = 0, jl = a.length; j < jl; j++, k++)
+            r[k] = a[j];
+    return r;
+};
 exports.__esModule = true;
 exports.CreatePostComponent = void 0;
 var core_1 = require("@angular/core");
 var forms_1 = require("@angular/forms");
 var CreatePostComponent = /** @class */ (function () {
-    function CreatePostComponent(fb) {
+    function CreatePostComponent(router, fb, postService) {
+        this.router = router;
         this.fb = fb;
+        this.postService = postService;
         this.errorMessage = '';
-        this.createForm = fb.group({
+        this.selectedImageIdx = 0;
+        this.imageFiles = [];
+        this.imageUrls = [];
+        this.createForm = this.fb.group({
             'title': ['', [forms_1.Validators.required]],
             'content': ['', [forms_1.Validators.required]],
-            'mainImage': [''],
+            'mainImage': ['', [forms_1.Validators.required]],
             'images': [''],
             'videos': ['']
         });
     }
+    CreatePostComponent.prototype.createPost = function () {
+        // this.createForm.get('images')?.setValue(this.imageFiles);
+        var _this = this;
+        var _a;
+        var newPost = new FormData();
+        newPost.append('title', this.createForm.value.title);
+        newPost.append('content', this.createForm.value.content);
+        newPost.append('mainImage', this.createForm.value.mainImage);
+        (_a = this.imageFiles) === null || _a === void 0 ? void 0 : _a.forEach(function (image) {
+            if (_this.imageFiles[_this.selectedImageIdx] !== image) {
+                newPost.append('images', image);
+            }
+        });
+        this.postService.add(newPost)
+            .subscribe(function (postId) { return _this.router.navigateByUrl("/detail/" + postId); });
+    };
+    CreatePostComponent.prototype.mainImageHandle = function (url) {
+        var _a, _b;
+        this.selectedElement = url;
+        var currentIndex = (_a = this.imageUrls) === null || _a === void 0 ? void 0 : _a.indexOf(url);
+        this.selectedImageIdx = currentIndex;
+        var currentFile = this.imageFiles[currentIndex];
+        (_b = this.createForm.get('mainImage')) === null || _b === void 0 ? void 0 : _b.setValue(currentFile);
+    };
+    CreatePostComponent.prototype.onFileChange = function (event) {
+        var _this = this;
+        __spreadArrays(event.target.files).forEach(function (file) {
+            var _a;
+            (_a = _this.imageFiles) === null || _a === void 0 ? void 0 : _a.push(file);
+            var reader = new FileReader();
+            reader.readAsDataURL(file);
+            reader.onload = function (_event) {
+                var _a;
+                (_a = _this.imageUrls) === null || _a === void 0 ? void 0 : _a.push(reader.result);
+            };
+        });
+    };
+    CreatePostComponent.prototype.isSelected = function (element) {
+        return element == this.selectedElement;
+    };
+    Object.defineProperty(CreatePostComponent.prototype, "username", {
+        get: function () {
+            return this.createForm.get('title');
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(CreatePostComponent.prototype, "content", {
+        get: function () {
+            return this.createForm.get('content');
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(CreatePostComponent.prototype, "mainImage", {
+        get: function () {
+            return this.createForm.get('mainImage');
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(CreatePostComponent.prototype, "images", {
+        get: function () {
+            return this.createForm.get('images');
+        },
+        enumerable: false,
+        configurable: true
+    });
+    Object.defineProperty(CreatePostComponent.prototype, "videos", {
+        get: function () {
+            return this.createForm.get('videos');
+        },
+        enumerable: false,
+        configurable: true
+    });
     CreatePostComponent = __decorate([
         core_1.Component({
             selector: 'app-create-post',
