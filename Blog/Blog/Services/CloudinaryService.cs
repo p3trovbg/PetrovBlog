8a19a94a1813aba2d33ccd8c63349100abd908bc
George <p3trovsit@gmail.com>
2023-02-20 20:11:27 +0200

    wrapped cloudinary library

---
+++ Blog/Blog/Services/CloudinaryService.cs
@@ -0,0 +1,43 @@
+﻿using CloudinaryDotNet;
+using CloudinaryDotNet.Actions;
+
+namespace Blog.Services
+{
+    public class CloudinaryService : ICloudinaryService
+    {
+        private const string ImageFolder = "images";
+        private const string VideoFolder = "videos";
+        private readonly Cloudinary cloudinary;
+
+        public CloudinaryService(Cloudinary cloudinary)
+        {
+            this.cloudinary = cloudinary;
+        }
+
+        public async Task<ImageUploadResult> UploadImageAsync(IFormFile image, string imageId)
+        {
+            using var stream = image.OpenReadStream();
+
+            var uploadParams = new ImageUploadParams()
+            {
+                File = new FileDescription(imageId, stream),
+                Folder = ImageFolder,
+            };
+
+            return await this.cloudinary.UploadAsync(uploadParams);
+        }
+
+        public async Task<VideoUploadResult> UploadVideoAsync(IFormFile video, string videoId)
+        {
+            using var stream = video.OpenReadStream();
+
+            var uploadParams = new VideoUploadParams()
+            {
+                File = new FileDescription(videoId, stream),
+                Folder = VideoFolder,
+            };
+
+            return await this.cloudinary.UploadAsync(uploadParams);
+        }
+    }
+}
---
+++ Blog/Blog/Services/ICloudinaryService.cs
@@ -0,0 +1,11 @@
+﻿using CloudinaryDotNet.Actions;
+
+namespace Blog.Services
+{
+    public interface ICloudinaryService
+    {
+        Task<ImageUploadResult> UploadImageAsync(IFormFile image, string imageId);
+
+        Task<VideoUploadResult> UploadVideoAsync(IFormFile video, string videoid);
+    }
+}
