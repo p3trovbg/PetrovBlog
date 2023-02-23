b656bf5cade6f6e36708334ceb7cec137f989d33
George <p3trovsit@gmail.com>
2023-02-23 19:55:20 +0200

    add PostId as nullable in image and video entity

---
+++ Blog/Blog/Migrations/20230223110743_MakePostPropertyNullableInImageAndVideoEntities.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blog.Migrations
+{
+    public partial class MakePostPropertyNullableInImageAndVideoEntities : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Images_Posts_PostId",
+                table: "Images");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Videos_Posts_PostId",
+                table: "Videos");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "PostId",
+                table: "Videos",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "PostId",
+                table: "Images",
+                type: "uniqueidentifier",
+                nullable: true,
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Images_Posts_PostId",
+                table: "Images",
+                column: "PostId",
+                principalTable: "Posts",
+                principalColumn: "Id");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Videos_Posts_PostId",
+                table: "Videos",
+                column: "PostId",
+                principalTable: "Posts",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Images_Posts_PostId",
+                table: "Images");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Videos_Posts_PostId",
+                table: "Videos");
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "PostId",
+                table: "Videos",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<Guid>(
+                name: "PostId",
+                table: "Images",
+                type: "uniqueidentifier",
+                nullable: false,
+                defaultValue: new Guid("00000000-0000-0000-0000-000000000000"),
+                oldClrType: typeof(Guid),
+                oldType: "uniqueidentifier",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Images_Posts_PostId",
+                table: "Images",
+                column: "PostId",
+                principalTable: "Posts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Videos_Posts_PostId",
+                table: "Videos",
+                column: "PostId",
+                principalTable: "Posts",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
