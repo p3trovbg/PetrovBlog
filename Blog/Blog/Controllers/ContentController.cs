b7288d727506cac5fcdad16c834858cf19923ec3
George <p3trovsit@gmail.com>
2023-02-23 19:55:41 +0200

    create content controller

---
+++ Blog/Blog/Controllers/ContentController.cs
@@ -0,0 +1,31 @@
+﻿using Blog.Models.PostContent;
+using Blog.Services;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Blog.Controllers
+{
+    [Authorize]
+    public class ContentController : ApiController
+    {
+        private readonly IImageService imageService;
+
+        public ContentController(IImageService imageService)
+        {
+            this.imageService = imageService;
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Upload([FromForm] UploadContentModel data)
+        {
+            if (data.ContentFiles == null)
+            {
+                return NotFound(data);
+            }
+
+            var images = await this.imageService.UploadImages<ContentViewModel>(data.ContentFiles!);
+
+            return this.Ok(images);
+        }
+    }
+}
