c7ae964112f15711874d6dbb062f770d27260770
George <p3trovsit@gmail.com>
2023-02-24 15:50:37 +0200

    refactoring of edit component

--- Blog/Blog/ClientApp/src/app/edit-post/edit-post.component.ts
+++ Blog/Blog/ClientApp/src/app/edit-post/edit-post.component.ts
@@ -1,7 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Router } from '@angular/router';
+import { stat } from 'fs';
+import { json } from 'stream/consumers';
+import { ContentService } from '../common/content.service';
 import { PostService } from '../common/post.service';
+import { IContent } from '../common/shared/content';
 import { IPostDetail } from '../common/shared/post-details';
 
 @Component({
@@ -14,24 +18,27 @@ export class EditPostComponent implements OnInit {
   errorMessage = '';
   isLoading = false;
   imageUrls: string[] = [];
+  newImagesFile: File[] = []
   targetPost?: IPostDetail;
-  
+
   constructor(
+    private contentService: ContentService,
     private router: Router,
     private route: ActivatedRoute,
     private fb: FormBuilder,
     private postService: PostService) {
-      this.editForm = this.fb.group({
-        'title': ['', [Validators.required]],
-        'content': ['', [Validators.required]],
-        'mainImage': ['', [Validators.required]],
-        'images': [''],
-        'videos': [''],
-      })
+    this.editForm = this.fb.group({
+      'id': [],
+      'title': ['', [Validators.required]],
+      'content': ['', [Validators.required]],
+      'mainImage': ['', [Validators.required]],
+      'images': [''],
+      'videos': [''],
+    })
   }
 
   ngOnInit(): void {
-      this.getPost();
+    this.getPost();
   }
 
   getPost() {
@@ -41,23 +48,34 @@ export class EditPostComponent implements OnInit {
     }
 
     this.postService.getById<IPostDetail>(postId!)
-    .subscribe((post) => {
-      this.targetPost = post;
-      this.editForm.setValue({
-        title: this.targetPost?.title,
-        content: this.targetPost?.content,
-        mainImage: this.targetPost?.mainImageUrl,
-        images: this.targetPost?.images,
-        videos: this.targetPost?.videos});
-    })
+      .subscribe((post) => {
+        this.targetPost = post;
+        this.editForm.setValue({
+          id: this.targetPost.id,
+          title: this.targetPost?.title,
+          content: this.targetPost?.content,
+          mainImage: this.targetPost?.mainImageUrl,
+          images: this.targetPost?.images,
+          videos: this.targetPost?.videos
+        });
+      })
   }
 
-  editPost() {}
+  editPost() {
+
+      this.editForm.get('images')?.setValue(this.targetPost?.images);
+      this.editForm.get('mainImage')?.setValue(this.targetPost?.mainImageUrl);
+        this.isLoading = true;
+        this.postService.edit(this.editForm).subscribe({
+         next:(postId) => {
+           this.router.navigate(['/detail/', postId]);
+           this.isLoading = false;
+         },
+         error:(er) => this.errorMessage = 'Invalid edit'
+       })
+  };
 
 
-  mainImageHandle(image: string) {
-    this.targetPost!.mainImageUrl = image;
-    this.editForm.get('mainImage')?.setValue(image);
   removeImageHandle(targetImage: any) {
     this.contentService.deleteImage(targetImage.id).subscribe(() => {
       const result = this.targetPost?.images.filter(image => image.id != targetImage.id);
@@ -65,8 +83,15 @@ export class EditPostComponent implements OnInit {
     })
   }
 
-  onFileChange(event: any) {
+  mainImageHandle(image: any) {
+    const lastedImageUrl = this.targetPost?.mainImageUrl;
+    this.targetPost!.mainImageUrl = image.url;
+    image.url = lastedImageUrl;
+  }
 
+  onFileChange(event: any) {
+    this.contentService.uploadImages<IContent[]>([...event.target.files])
+      .subscribe(x => this.targetPost?.images.push(...x))
   }
 
   isSelected(image: string): boolean {
@@ -77,7 +102,7 @@ export class EditPostComponent implements OnInit {
   get username() {
     return this.editForm.get('title');
   }
-  
+
   get content() {
     return this.editForm.get('content');
   }
